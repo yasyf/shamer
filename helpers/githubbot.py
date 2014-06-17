1eb36744882fda7ac2261d356da59a4622fb70fb
Yasyf Mohamedali <ymohamedali@localytics.com>
2014-06-17 11:22:12 -0400

    edit old comment if it already exists

--- helpers/githubbot.py
+++ helpers/githubbot.py
@@ -3,16 +3,22 @@ from github import Github
 class GithubBot():
   def __init__(self, org, repo, token, storage=None):
     self.g = Github(token)
+    self.user = self.g.get_user()
     self.org = self.g.get_organization(org)
     self.repo = self.org.get_repo(repo)
-    # list-like object, could be persistent
-    self.commented = storage if storage else []
   
+  def past_comment(self, pr):
+    for comment in pr.get_issue_comments():
+      if comment.user.id == self.user.id:
+        return comment
+
   def comment(self, pull_request_id, message, url):
-    if pull_request_id not in self.commented:
-      self.commented.append(pull_request_id)
-      pr = self.repo.get_pull(pull_request_id)
-      body = "{}: [{}]({})".format(message, pr.title, url)
+    pr = self.repo.get_pull(pull_request_id)
+    body = "{}: [{}]({})".format(message, pr.title, url)
+    past_comment = self.past_comment(pr)
+    if past_comment:
+      past_comment.edit(body)
+    else:
       pr.create_issue_comment(body)
 
   def get_pr_by_branch(self, branch_name):
