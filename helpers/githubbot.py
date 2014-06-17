4d1eabf6b201eba6be387c11d17edb12b0a17bf1
Yasyf Mohamedali <ymohamedali@localytics.com>
2014-06-16 20:55:41 -0400

    prevent commenting on same PR multiple times

--- helpers/githubbot.py
+++ helpers/githubbot.py
@@ -1,12 +1,16 @@
 from github import Github
 
 class GithubBot():
-  def __init__(self, org, repo, token):
+  def __init__(self, org, repo, token, storage=None):
     self.g = Github(token)
     self.org = self.g.get_organization(org)
     self.repo = self.org.get_repo(repo)
+    # list-like object, could be persistent
+    self.commented = storage if storage else []
   
   def comment(self, pull_request_id, message, url):
-    pr = self.repo.get_pull(pull_request_id)
-    body = "{}: [{}]({})".format(message, pr.title, url)
-    pr.create_issue_comment(body)
+    if pull_request_id not in self.commented:
+      self.commented.append(pull_request_id)
+      pr = self.repo.get_pull(pull_request_id)
+      body = "{}: [{}]({})".format(message, pr.title, url)
+      pr.create_issue_comment(body)
